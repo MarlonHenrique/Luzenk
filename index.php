38b9db89b64ec231d3c1396a3fdbf3acebc5842a
MarlonHenrique <marlonhenrique@niggabyte.com.br>
2016-04-16 12:02:27 -0300

    Adicionado ao site a parte de portifolio

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
  		<p class="sobre-home">
  			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore magni dolor consequuntur odit, minima nam optio autem reiciendis? Corporis quam error perferendis, repudiandae nemo dolor qui nostrum non delectus maiores.
  			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, dignissimos, sit earum id asperiores ex harum. Eveniet aliquid in aperiam assumenda odio quod, eum, laboriosam alias veniam enim dolorum! Esse.
+ 			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, maiores quasi. Officiis odit similique incidunt aut, laudantium et ea officia. Debitis fugit odio quam consectetur quia similique ex alias, commodi.
  		</p>
  	</div>
  </section>
@@ -37,7 +38,14 @@
 <!--Portifolio-->
 <section id="portifolio">
  	<div class="portifolio-home">
- 		
+ 		<div class="container">
+ 			<?php $cont = 0; while ($cont <= 5): ?>
+ 				<div class="col-md-3" id="caixas-portifolio">
+ 					<a href="#" id="port-home"><img src="img/content/foto-perfil.jpg" alt="foto portifolio" id="foto-portifolio"></a>
+ 					<h3 id="nome-projeto-portifolio">Nome do projeto!</h3>
+ 				</div>
+ 			<?php $cont++; endwhile; ?>	
+ 		</div>
  	</div>
  </section>
 <!--Fim portifolio--> 
